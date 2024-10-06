4bf82d2d454aeb83293768b5e4fdc7fa2c9eb1ef
Safeya-Yasien <ssafeya5@gmail.com>
2024-10-06 21:59:32 +0300

    add replace to prevent arrow back to error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -9,6 +9,7 @@ const Error = () => {
       </p>
       <Link
         to="/"
+        replace
         className="mt-6 inline-block px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
       >
         Go Back Home
