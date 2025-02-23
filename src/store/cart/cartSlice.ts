9d028db5e542d856de5968a88d585750192fde40
Safeya-Yasien <ssafeya5@gmail.com>
2025-02-23 17:04:16 +0200

    update Products.tsx, actGetProductsByItems.ts, cartSlice.ts and actGetCategories.ts

--- src/store/cart/cartSlice.ts
+++ src/store/cart/cartSlice.ts
@@ -27,49 +27,9 @@ const cartSlice = createSlice({
         state.items[id] = 1;
       }
     },
-    cartItemChangeQuantity: (state, action) => {
-      state.items[action.payload.id] = action.payload.quantity;
-    },
-    cartItemRemove: (state, action) => {
-      delete state.items[action.payload];
-      state.productsFullInfo = state.productsFullInfo.filter(
-        (el) => el.id !== action.payload
-      );
-    },
-    cleanCartProductsFullInfo: (state) => {
-      state.productsFullInfo = [];
-    },
-    clearCartAfterPlaceOrder: (state) => {
-      state.items = {};
-      state.productsFullInfo = [];
-    },
   },
-  //   extraReducers: (builder) => {
-  //     builder.addCase(actGetProductsByItems.pending, (state) => {
-  //       state.loading = "pending";
-  //       state.error = null;
-  //     });
-  //     builder.addCase(actGetProductsByItems.fulfilled, (state, action) => {
-  //       state.loading = "succeeded";
-  //       state.productsFullInfo = action.payload;
-  //     });
-  //     builder.addCase(actGetProductsByItems.rejected, (state, action) => {
-  //       state.loading = "failed";
-  //       if (isString(action.payload)) {
-  //         state.error = action.payload;
-  //       }
-  //     });
-  //   },
 });
 
-// export { getCartTotalQuantitySelector, actGetProductsByItems };
-
-export const {
-  addToCart,
-  cartItemChangeQuantity,
-  cartItemRemove,
-  cleanCartProductsFullInfo,
-  clearCartAfterPlaceOrder,
-} = cartSlice.actions;
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
