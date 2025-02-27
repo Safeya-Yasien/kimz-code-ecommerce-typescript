063bc8b1d9402f957096b40befdc044d70181c07
Safeya-Yasien <ssafeya5@gmail.com>
2025-02-27 23:15:00 +0200

    update MainLayout.tsx remove h-full style

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div className="w-full  flex flex-col ">
+    <div className="w-full flex flex-col ">
       <Header />
-      <main className="flex-1 overflow-auto pt-16">
+      <main className="flex-1 overflow-auto pt-16 ">
         <Outlet />
       </main>
       <Footer />
