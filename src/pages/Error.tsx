aeaec1f23b4e29e9a0db2d3616a761af6afb4ac4
Safeya-Yasien <ssafeya5@gmail.com>
2025-03-02 22:21:44 +0200

    using lottie animation in Error.tsx page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,23 +1,13 @@
-import { isRouteErrorResponse, Link, useRouteError } from "react-router";
+import { Link } from "react-router";
+import Lottie from "lottie-react";
+
+import notFound from "@assets/lottieFiles/notFound.json";
 
 const Error = () => {
-  const error = useRouteError();
-
-  let errorStatus: number;
-  let errorStatusText: string;
-
-  if (isRouteErrorResponse(error)) {
-    errorStatus = error.status;
-    errorStatusText = error.statusText;
-  } else {
-    errorStatus = 404;
-    errorStatusText = "Page not found";
-  }
-
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-red-500">{errorStatus}</h1>
-      <p className="text-xl text-gray-700 mt-2">{errorStatusText}</p>
+      <Lottie animationData={notFound} />
+
       <Link
         to="/"
         className="mt-6 px-6 py-2 bg-blue-600 text-white text-lg rounded-lg shadow hover:bg-blue-700 transition"
