6f0d9f643f729e5c4729f22be6e33757d77e4bd5
Safeya-Yasien <ssafeya5@gmail.com>
2025-03-07 13:02:51 +0200

    update LottieHandler.tsx change link url

--- src/components/feedback/LottieHandler/LottieHandler.tsx
+++ src/components/feedback/LottieHandler/LottieHandler.tsx
@@ -30,7 +30,7 @@ const LottieHandler = ({ type, message }: TLottieHandlerProps) => {
       {message &&
         (["notFound", "empty", "error"].includes(type) ? (
           <Link
-            to={"/categories"}
+            to={"/"}
             className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
           >
             {message}
